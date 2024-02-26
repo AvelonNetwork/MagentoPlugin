e45f10e10e510e283ce2761b8be8de41ad6ea6e9
Datty <datty@pixable.co>
2024-02-26 11:46:14 +0000

    no message

--- Avelon/AvelonNetwork/Controller/Adminhtml/Settings/Index.php
+++ Avelon/AvelonNetwork/Controller/Adminhtml/Settings/Index.php
@@ -1,19 +1,16 @@
 <?php
 /**
- * BSS Commerce Co.
+ * Avelon Network.
  *
  * NOTICE OF LICENSE
  *
- * This source file is subject to the EULA
- * that is bundled with this package in the file LICENSE.txt.
- * It is also available through the world-wide-web at this URL:
- * http://bsscommerce.com/Bss-Commerce-License.txt
+
  *
- * @category   BSS
- * @package    Bss_CreateMenuBackend
- * @author     Extension Team
- * @copyright  Copyright (c) 2017-2018 BSS Commerce Co. ( http://bsscommerce.com )
- * @license    http://bsscommerce.com/Bss-Commerce-License.txt
+ * @category   Avelon
+ * @package    Avelon_AvelonNetwork
+ * @author     Avelon
+ * @copyright  Copyright (c) Avelon Network ( https://avelonetwork.com/ )
+ * @license    https://avelonetwork.com/
  */
 namespace Avelon\AvelonNetwork\Controller\Adminhtml\Settings;
 
